914960703e8e6fb0996707f7a3e3a857bc3034a1
wang-hd <wang_hd@sjtu.edu.cn>
2022-01-14 17:21:49 -0500

    tutorial

---
+++ Assets/Scripts/ArrowKeyMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowKeyMovement : MonoBehaviour
+{
+
+    public float Movement_speed = 4;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 current_input = GetInput();
+        rb.velocity = current_input * Movement_speed;
+    }
+
+    Vector2 GetInput() 
+    {
+        float horizontal_input = Input.GetAxisRaw("Horizontal");
+        float vertical_input = Input.GetAxisRaw("Vertical");
+        if (Mathf.Abs(horizontal_input) > 0.0f) {
+            vertical_input = 0.0f;
+        }
+
+        return new Vector2(horizontal_input, vertical_input);
+    }
+}
---
+++ Assets/Scripts/Collector.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collector : MonoBehaviour
+{
+    public AudioClip rupee_collection_sound_clip;
+    Inventory inventory;
+
+    void Start() 
+    {
+        inventory = GetComponent<Inventory>();
+        if (inventory == null) {
+            Debug.LogWarning("WARNING: Gameobject with a collider has no inventory to store things in!");
+        }
+    }
+
+    void OnTriggerEnter(Collider coll) 
+    {
+        GameObject object_collide_with = coll.gameObject;
+        
+        if (object_collide_with.tag == "rupee")
+        {
+            if (inventory != null)
+                inventory.Add_rupees(1);
+            Destroy(object_collide_with);
+
+            AudioSource.PlayClipAtPoint (rupee_collection_sound_clip, Camera.main.transform.position);
+        }
+    }
+}
